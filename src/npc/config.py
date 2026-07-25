8a9d4089a92f65f95f6a7ab835220908080dc946
Alexey Nilov <alexeyn00000@gmail.com>
2026-07-25 23:20:57 +0200

    fix

--- src/npc/config.py
+++ src/npc/config.py
@@ -1,5 +1,5 @@
 import os
 
-LLM_BASE_URL = os.getenv("LLM_BASE_URL", "http://127.0.0.1:12345/v1")
+LLM_BASE_URL = os.getenv("LLM_BASE_URL", "http://127.0.0.1:12345")
 LLM_API_KEY = os.getenv("LLM_API_KEY", "key")
 LLM_NAME = os.getenv("LLM_NAME", "gemma-4-12b")
